c431fda05ca1fca79758cb71b6a8a2df3f9f70e6
Izaelle <izaelleavrillavigne@gmail.com>
2025-08-19 16:02:54 -0300

    fix: animation time

--- src/components/nav-link-social-media.tsx
+++ src/components/nav-link-social-media.tsx
@@ -8,28 +8,28 @@ export function SocialMedia() {
   return (
     <div className="flex gap-8 items-center justify-center">
       <Link href={""}>
-        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center cursor-pointer">
           <LinkedinLogo size={20} />
           LinkedIn
         </Button>
       </Link>
 
       <Link href={""}>
-        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center cursor-pointer">
           <WhatsappLogo size={20} />
           Whatsapp
         </Button>
       </Link>
 
       <Link href={""}>
-        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center cursor-pointer">
           <Envelope size={20} />
           Email
         </Button>
       </Link>
 
       <Link href={""}>
-        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center cursor-pointer">
           <GithubLogo size={20} />
           Github
         </Button>
