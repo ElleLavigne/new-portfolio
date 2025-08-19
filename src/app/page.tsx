83de74b136333c8abca67fc12d5fe29446162302
Izaelle <izaelleavrillavigne@gmail.com>
2025-08-19 15:58:41 -0300

    feat: home / link social media / animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,45 @@
+"use client";
 import { Header } from "@/components/header";
+import { SocialMedia } from "@/components/nav-link-social-media";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col container mx-auto justify-center items-center">
       <Header />
-      <div className="container mx-auto flex flex-col max-w-[907px] items-center text-center gap-8">
-        <h1 className="font-medium text-5xl text-zinc-950">Olá, sou a Izaelle Lavigne, frontend e UI Design</h1>
-        <p className="text-zinc-600">
-          Utilizo JavaScript, React.js e Vue.js. Tenho experiência na criação de
-          interfaces para Apps e sites, unindo desenvolvimento e design. Utilizo
-          o Figma como principal ferramenta para elaborar protótipos e designs
-          focados em usabilidade e experiência do usuário.
-        </p>
+      <div className="flex flex-col gap-14 mt-72">
+        <div className="flex flex-col max-w-[907px] items-center text-center gap-8">
+          <motion.h1
+            className="font-medium text-5xl text-zinc-950"
+            initial={{ opacity: 0, y: 30 }}      // começa invisível e mais abaixo
+            animate={{ opacity: 1, y: 0 }}        // aparece e sobe
+            transition={{ duration: 0.8, delay: 0.2 }} // duração e atraso
+          >
+            Olá, sou a Izaelle Lavigne, frontend e UI Design
+          </motion.h1>
+
+          <motion.p
+            className="text-zinc-600"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          >
+            Utilizo JavaScript, React.js e Vue.js. Tenho experiência na criação
+            de interfaces para Apps e sites, unindo desenvolvimento e design.
+            Utilizo o Figma como principal ferramenta para elaborar protótipos e
+            designs focados em usabilidade e experiência do usuário.
+          </motion.p>
+        </div>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.8 }}
+        >
+          <SocialMedia />
+        </motion.div>
       </div>
     </div>
   );
 }
+
--- src/components/nav-link-social-media.tsx
+++ src/components/nav-link-social-media.tsx
@@ -1,17 +1,39 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { LinkedinLogo } from "phosphor-react";
+import { Envelope, GithubLogo, LinkedinLogo, WhatsappLogo } from "phosphor-react";
 
-export function SocialMedia(){
-    return(
-        <div>
-            <Link className="" href={""}>
-            <Button
-            className=""
-            >
-                <LinkedinLogo/>
-            </Button>
-            </Link>
-        </div>
-    )
+export function SocialMedia() {
+  return (
+    <div className="flex gap-8 items-center justify-center">
+      <Link href={""}>
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+          <LinkedinLogo size={20} />
+          LinkedIn
+        </Button>
+      </Link>
+
+      <Link href={""}>
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+          <WhatsappLogo size={20} />
+          Whatsapp
+        </Button>
+      </Link>
+
+      <Link href={""}>
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+          <Envelope size={20} />
+          Email
+        </Button>
+      </Link>
+
+      <Link href={""}>
+        <Button className="shine text-zinc-950 hover:bg-zinc-200 rounded-3xl flex gap-2 items-center">
+          <GithubLogo size={20} />
+          Github
+        </Button>
+      </Link>
+    </div>
+  );
 }
